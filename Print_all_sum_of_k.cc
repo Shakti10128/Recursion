49c89a51f7439f590e80fd91c59b132413ff396f
shakti <kumarshakti2017@gmail.com>
2023-03-16 21:48:49 +0530

    nothing change

--- Print_all_sum_of_k.cc
+++ Print_all_sum_of_k.cc
@@ -2,6 +2,7 @@
 using namespace std;
 
 void Print_all_sum_of_k(vector<int> arr,vector<int>&ans,int size,int i,int k,int sum){
+    // base case
     if(i >= size){
         if(sum == k){
             for(auto it: ans){
@@ -21,6 +22,25 @@ void Print_all_sum_of_k(vector<int> arr,vector<int>&ans,int size,int i,int k,int
     Print_all_sum_of_k(arr,ans,size,i+1,k,sum);
 }
 
+/*counting the numbers of sum k in presenting array*/
+
+// int Print_all_sum_of_k(vector<int> arr,int size,int i,int k,int sum){
+//     // base case
+//     if(i >= size){
+//         if(sum == k){
+//             return 1;
+//         }
+//         return 0;
+//     }
+//     // include
+//     sum+=arr[i];
+//     int ans1 = Print_all_sum_of_k(arr,size,i+1,k,sum);
+//     // exclude
+//     sum-=arr[i];
+//     int ans2 = Print_all_sum_of_k(arr,size,i+1,k,sum);
+//     return ans1+ans2;
+// }
+
 int main(){
     vector<int> arr = {1,2,1};
     int size = arr.size();
@@ -29,4 +49,7 @@ int main(){
     vector<int> ans;
     int sum = 0;
     Print_all_sum_of_k(arr,ans,size,i,k,sum);
+
+    /*counting the numbers of sum k in presenting array*/
+    // cout<<Print_all_sum_of_k(arr,size,i,k,sum);
 }
