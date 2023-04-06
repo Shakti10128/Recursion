4b38ee82b01b9ef8c539154c3535dde3bddda43e
Shakti Kumar <106401158+Shakti10128@users.noreply.github.com>
2023-04-06 22:46:28 +0530

    Create mergeSort.cc

---
+++ mergeSort.cc
@@ -0,0 +1,39 @@
+    void merge(int arr[], int s, int mid, int e)
+    {
+         int i = s,j = mid+1;
+         int index = 0;
+         vector<int> a;
+         while(i<=mid && j<=e){
+             if(arr[i] <= arr[j]){
+               a.push_back(arr[i++]);
+             }
+             else{
+                 a.push_back(arr[j++]);
+             }
+         }
+         while(i <= mid){
+            a.push_back(arr[i++]);
+         }
+         while(j<=e){
+             a.push_back(arr[j++]);
+             
+         }
+        
+        for(int i = s;i<=e;i++){
+            arr[i] = a[i-s];
+        }
+    }
+    void mergeSort(int arr[], int s, int e)
+    {
+        // base case
+        if(s == e){
+            return;
+        }
+        int mid = (s+e)/2;
+        // left array
+        mergeSort(arr,s,mid);
+        // right
+        mergeSort(arr,mid+1,e);
+        // merge function calling
+        merge(arr,s,mid,e);
+    }
